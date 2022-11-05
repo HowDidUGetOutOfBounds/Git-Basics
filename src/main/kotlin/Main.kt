790b47a3a83ee875a5eff0ca987bc4e81ecc4c98
Sergey <serg.karpik@gmail.com>
2022-11-05 20:07:21 +0300

    added task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,29 @@
 fun main() {
     doTask1()
+    println(doTask2(5))
 }
 
-fun doTask1(){
+fun doTask1() {
     val a = readLine()?.toIntOrNull()
-    if(a != null) {
+    if (a != null) {
         if (a % 2 == 1) {
             println("Not even")
         } else {
             println("Even")
         }
     }
+}
+
+fun doTask2(data: Int): Int {
+    if (data < 0)
+        return -1
+
+    if (data == 0)
+        return 1
+
+    var res = 1
+    for (i in 1..data) {
+        res *= i
+    }
+    return res
 }
