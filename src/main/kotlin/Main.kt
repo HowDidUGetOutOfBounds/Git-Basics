ad52e4098682f8856a3fdb4ca33b0cc5279310fc
Sergey <serg.karpik@gmail.com>
2022-11-05 19:43:42 +0300

    added my task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+    //check if even
+    val a = readLine()?.toInt()
+    if (a!! % 2 == 1) {
+        println("Not even")
+    }
+    else
+    {
+        println("Even")
+    }
 }
